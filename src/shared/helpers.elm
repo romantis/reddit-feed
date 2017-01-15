ff99c1c7a7db23f22a671fc4d7ecd04f7cc905cc
Roman Tischenko <udatos@gmail.com>
2017-01-15 18:20:35 +0200

    Add helper functions

--- src/shared/helpers.elm
+++ src/shared/helpers.elm
@@ -1,7 +1,7 @@
 module Shared.Helpers exposing (..)
 
 import Html exposing (Attribute)
-import Html.Events exposing (onWithOptions, defaultOptions)
+import Html.Events exposing (onWithOptions, defaultOptions, keyCode, on)
 import Json.Decode as Json exposing (succeed)
 
 hrefClick : (String -> msg) -> String -> Attribute msg
@@ -10,3 +10,19 @@ hrefClick msg url =
         "click"  
         {defaultOptions | preventDefault = True }
         (succeed (msg url))
+
+
+onEnter : msg -> Attribute msg
+onEnter msg =
+    let
+        isEnter code =
+            if code == 13 then
+                Json.succeed msg
+            else
+                Json.fail "not ENTER"
+    in
+        on "keydown" (Json.andThen isEnter keyCode)
+
+
+(=>) : a -> b -> ( a, b )
+(=>) = (,)
